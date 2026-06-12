b9cb2b4c0f45f96cdd6dea3b28a168390af2f510
Noor Fatima <noor2239064@gmail.com>
2026-06-12 14:26:43 +0500

    api fetched

--- src/UI/Products.jsx
+++ src/UI/Products.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+
+//we call the api url outside the main function so that the api loads only once and not everytime the functions
+const url = "https://dummyjson.com/products"
 export default function Products() {
+    const [ value, setValue ] = useState([]);
+    async function callApi () {
+        const apiData = await fetch(url);
+        const res = await apiData.json();
+        setValue(res.products);
+    }
+
+    useEffect(()=>
+    {
+        callApi();
+    },[])
+
+    useEffect(()=>{
+        console.log(value)
+    },[value])
+
+
   return (
     <div>
-      
+    
     </div>
   )
 }
